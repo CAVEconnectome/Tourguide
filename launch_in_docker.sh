36a974b69e1161f65f3fc0d7e4e0c2a1aab95d85
Casey Schneider-Mizell <caseysm@gmail.com>
2025-02-12 15:07:35 -0800

    make launch-in-docker generic

--- launch_in_docker.sh
+++ launch_in_docker.sh
@@ -2,5 +2,5 @@
 docker run \
     --env-file .env.debug \
     -p 8080:8080 \
-    -v /Users/caseysm/.cloudvolume/secrets:/root/.cloudvolume/secrets \
+    -v ${HOME}/.cloudvolume/secrets:/root/.cloudvolume/secrets \
     gbv2-testing
