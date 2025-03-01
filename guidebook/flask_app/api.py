162673f2ac09b93aa95793a7aa7ead8c5f9e99ec
Casey Schneider-Mizell <caseysm@gmail.com>
2025-02-28 18:46:38 -0800

    v2.0.7

--- guidebook/flask_app/api.py
+++ guidebook/flask_app/api.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, g
 from .config import GUIDEBOOK_PREFIX
 
 api_bp = Blueprint("main", __name__)
-__version__ = "2.0.6"
+__version__ = "2.0.7"
 
 
 @api_bp.route("/")
