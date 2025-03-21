f6a0732ae3253308a0bd84e2916aa9a33f87dd95
Casey Schneider-Mizell <caseysm@gmail.com>
2025-03-20 21:13:11 -0700

    v2.2.3

--- tourguide/flask_app/api.py
+++ tourguide/flask_app/api.py
@@ -7,7 +7,7 @@ from ..tourguide_lib.lib_utils import make_global_client
 from .config import TOURGUIDE_PREFIX
 
 api_bp = Blueprint("main", __name__)
-__version__ = "2.2.2"
+__version__ = "2.2.3"
 
 cache = Cache(
     config={
