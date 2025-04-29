ce293678954a9f2e16c2adfc3a49d730256fc9d6
Casey Schneider-Mizell <caseysm@gmail.com>
2025-04-29 17:14:16 -0400

    Revert "twiddles"
    This reverts commit 32a3eba40a44117edec127978f1ad47cda87f5b4.

--- tourguide/tourguide_app/pages/callbacks.py
+++ tourguide/tourguide_app/pages/callbacks.py
@@ -295,7 +295,7 @@ def register_callbacks(app):
     def set_image_mirror(search, image_mirror):
         new_image_mirror = image_mirror_from_url_query(search)
         if new_image_mirror is not None:
-            return new_image_mirror
+            return image_mirror_from_url_query(search)
         else:
             raise PreventUpdate
 
