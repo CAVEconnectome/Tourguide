821aad0a97d8876a8ce7d06b8749abdec3d98510
Casey Schneider-Mizell <caseysm@gmail.com>
2025-04-30 13:20:30 -0400

    let dmc be dmc

--- tourguide/tourguide_app/__init__.py
+++ tourguide/tourguide_app/__init__.py
@@ -1,16 +1,10 @@
 import flask
-import dash
-from dash import Dash, _dash_renderer
-
-# Required for dash mantine components
-_dash_renderer._set_react_version("18.2.0")
+from dash import Dash
 
 import dash_mantine_components as dmc
 
-from .external_stylesheets import external_stylesheets
 from .pages.callbacks import register_callbacks
 import pathlib
-import os
 
 pages_folder = pathlib.Path(__file__).parent.absolute().joinpath("pages")
 
@@ -19,7 +13,6 @@ def create_tourguide_app(name=__name__, config={}, **kwargs):
     dapp = Dash(
         name,
         use_pages=True,
-        external_stylesheets=external_stylesheets,
         pages_folder=pages_folder,
         **kwargs,
     )
